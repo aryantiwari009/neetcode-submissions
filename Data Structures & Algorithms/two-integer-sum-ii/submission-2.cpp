ee1a16a0f59ca465d9c98f062f3381a1938fa2ee
ARYAN TIWARI <96858602+aryantiwari009@users.noreply.github.com>
2026-07-02 13:55:34 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> output;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                if(nums[i] < nums[j] && nums[i] != nums[j])
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        output.push_back(i + 1);
+                        output.push_back(j + 1);
+                    }
+                }
+            }
+        }
+
+        return output;
+    }
+};
