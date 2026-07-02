7284c3feda8ea24b9185815a07e62f375b0ee483
ARYAN TIWARI <96858602+aryantiwari009@users.noreply.github.com>
2026-07-02 19:23:02 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int maxArea = 0;
+        int currentMaxArea = 0;
+
+        int left = 0;
+        int right = h.size() - 1;
+
+        while(left < right){
+            currentMaxArea = min(h[left], h[right]) * (right - left);
+
+            if(h[left] <= h[right]) left++;
+            else if(h[left] > h[right]) right--;
+
+            maxArea = max(maxArea, currentMaxArea);
+        }
+
+        return maxArea;
+    }
+};
